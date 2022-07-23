579f1fc6f543bbd15c88e9abca5c61b8c15e28c5
Perseverance <1445272085@qq.com>
2022-07-23 11:20:44 +0800

    Swagger相关配置

--- reggie_take_out/src/main/java/com/example/reggie/config/SpringMvcConfig.java
+++ reggie_take_out/src/main/java/com/example/reggie/config/SpringMvcConfig.java
@@ -2,13 +2,24 @@ package com.example.reggie.config;
 
 import com.example.reggie.common.JacksonObjectMapper;
 import com.example.reggie.interceptor.LoginCheckInterceptor;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
@@ -17,8 +28,11 @@ import java.util.List;
  */
 
 //extends WebMvcConfigurationSupport 会拦截静态资源，用implements WebMvcConfigurer
+// public class SpringMvcConfig extends WebMvcConfigurationSupport {
 @Slf4j
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
 public class SpringMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginCheckInterceptor interceptor;
@@ -35,9 +49,26 @@ public class SpringMvcConfig implements WebMvcConfigurer {
                         "/backend/**",
                         "/front/**",
                         "/common/**",
-                        "/user/sendMsg",  //移动端发送短信
-                        "/user/login");  //移动端登录
+                        "/user/sendMsg",    //移动端发送短信
+                        "/user/login",      //移动端登录
+                        "/doc.html",        //4个Swagger相关配置
+                        "/webjars/**",
+                        "/swagger-resources",
+                        "/v2/api-docs");
     }
+
+//    /**
+//     * 设置静态资源映射
+//     * @param registry
+//     */
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        log.info("开始进行静态资源映射...");
+//        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+//        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+//    }
 
     /**
      * 扩展springmvc框架的消息转换器
@@ -54,4 +85,24 @@ public class SpringMvcConfig implements WebMvcConfigurer {
         //index = 0 代表设置在第一个
         converters.add(0,mjhmc);
     }
+
+    @Bean
+    public Docket createRestApi() {
+        // 文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.reggie.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("瑞吉外卖")
+                .version("1.0")
+                .description("瑞吉外卖接口文档")
+                .build();
+    }
+
 }
